fea6895ddb12ca00e7dfc06bf14187ab46a95871
Joodj <jood_joy@mail.ru>
2024-04-17 17:16:14 +0300

    update

--- src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
@@ -1,16 +1,10 @@
 package com.javarush.telegrambot;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     public static final String NAME = "GameJavaRush_bot"; // TODO: добавьте имя бота в кавычках
@@ -22,60 +16,98 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update updateEvent) {
-        if (updateEvent.hasMessage() && updateEvent.getMessage().hasText()) {
-            String messageText = updateEvent.getMessage().getText();
-            String firstName = updateEvent.getMessage().getFrom().getFirstName();
-
-            if (messageText.equals("/bye")) {
-                sendTextMessageAsync("Asta la vista, baby!");
-            } else if (messageText.equals("/start")) {
-                String greetingMessage = "*Привет*, будущий программист " + firstName + "!";
-                sendTextMessageAsync(greetingMessage);
-
-                List<KeyboardButton> buttons = new ArrayList<>();
-                buttons.add(new KeyboardButton("Кот"));
-                buttons.add(new KeyboardButton("Собака"));
-
-                sendTextMessageWithButtons("Ваше любимое животное?", buttons, updateEvent.getMessage().getChatId().toString());
-            }
-        }
-
         if (updateEvent.hasCallbackQuery()) {
-            String chosenAnimal = updateEvent.getCallbackQuery().getData();
-            if (chosenAnimal.equals("Кот")) {
-                sendImageMessageAsync("src/main/resources/images/step_4_pic.jpg");
-            } else if (chosenAnimal.equals("Собака")) {
-                sendImageMessageAsync("src/main/resources/images/step_6_pic.jpg");
-            }
+            handleCallbackQuery(updateEvent);
+        } else if (updateEvent.hasMessage() && updateEvent.getMessage().hasText()) {
+            handleMessage(updateEvent);
         }
     }
 
-    private void sendTextMessageWithButtons(String messageText, List<KeyboardButton> buttons, String chatId) {
-        SendMessage message = new SendMessage();
-        message.setText(messageText);
-        message.setChatId(chatId);
+    private void handleCallbackQuery(Update updateEvent) {
+        String callbackData = updateEvent.getCallbackQuery().getData();
 
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        keyboardMarkup.setResizeKeyboard(true);
-        keyboardMarkup.setOneTimeKeyboard(true);
+        if (callbackData.equals("yes")) {
+            String step1Message = TelegramBotContent.STEP_1_TEXT;
+            sendTextMessageAsync(step1Message);
+            sendImageMessageAsync("src/main/resources/images/step_1_pic.jpg");
+            String message1 = "Твои действия?";
+            sendTextMessageAsync(message1, Map.of("Взламываем холодильник", "Hack_the_refrigerator", "Отступаем", "We_retreat"));
 
-        List<KeyboardRow> keyboard = new ArrayList<>();
-        KeyboardRow row = new KeyboardRow();
+        } else if (callbackData.equals("Hack_the_refrigerator")) {
+            String step2Message = TelegramBotContent.STEP_2_TEXT;
+            sendTextMessageAsync(step2Message);
+            sendImageMessageAsync("src/main/resources/images/step_2_pic.jpg");
+            String step3Message = TelegramBotContent.STEP_3_TEXT;
+            sendTextMessageAsync(step3Message);
+            sendImageMessageAsync("src/main/resources/images/step_3_pic.jpg");
 
-        for (KeyboardButton button : buttons) {
-            row.add(button);
-        }
-        keyboard.add(row);
-        keyboardMarkup.setKeyboard(keyboard);
-        message.setReplyMarkup(keyboardMarkup);
+            String message2 = "Твои действия?";
+            sendTextMessageAsync(message2, Map.of("Отправить робопылесос за едой!", "Send_a_robot_vacuum_cleaner_for_food!", "Проехаться на робопылесосе!",
+                    "Ride_a_robot_vacuum_cleaner!", "Убегать от робопылесоса!", "Run_away_from_the_robot_vacuum_cleaner!"));
 
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+        } else if (callbackData.equals("We_retreat")) {
+            String step10Message = TelegramBotContent.GAME_OVER;
+            sendTextMessageAsync(step10Message);
+            sendImageMessageAsync("src/main/resources/images/gameover.jpg");
+
+
+        } else if (callbackData.equals("Ride_a_robot_vacuum_cleaner!")) {
+            String step4Message = TelegramBotContent.STEP_4_TEXT;
+            sendTextMessageAsync(step4Message);
+            sendImageMessageAsync("src/main/resources/images/step_4_pic.jpg");
+
+            String step5Message = TelegramBotContent.STEP_5_TEXT;
+            sendTextMessageAsync(step5Message);
+            sendImageMessageAsync("src/main/resources/images/step_5_pic.jpg");
+
+            String message3 = "Твои действия?";
+            sendTextMessageAsync(message3, Map.of("Включить", "Turn_on", "Игнорировать", "Ignore"));
+
+        } else if (callbackData.equals("Turn_on")) {
+            String step6Message = TelegramBotContent.STEP_6_TEXT;
+            sendTextMessageAsync(step6Message);
+            sendImageMessageAsync("src/main/resources/images/step_6_pic.jpg");
+            String message3 = "Идём дальше?";
+            sendTextMessageAsync(message3, Map.of("Да, идём", "Yes_lets_go", "Нет, не идём", "No_we_are_not_going"));
+
+        } else if (callbackData.equals("Yes_lets_go")) {
+            String step7Message = TelegramBotContent.STEP_7_TEXT;
+            sendTextMessageAsync(step7Message);
+            sendImageMessageAsync("src/main/resources/images/step_7_pic.jpg");
+            String message4 = "Взламываем?";
+            sendTextMessageAsync(message4, Map.of("Взламываем", "Hack", "Нет, я же котик и у меня лапки", "No_I_am_a_cat_and_I_have_paws"));
+
+        } else if (callbackData.equals("Hack")) {
+            String step8Message = TelegramBotContent.STEP_8_TEXT;
+            sendTextMessageAsync(step8Message);
+            sendImageMessageAsync("src/main/resources/images/step_8_pic.jpg");
+            String message3 = "Идём дальше?";
+            sendTextMessageAsync(message3, Map.of("Да", "Yes", "Нет", "No"));
+
+        } else if (callbackData.equals("Yes")) {
+            String step9Message = TelegramBotContent.FINAL_TEXT;
+            sendTextMessageAsync(step9Message);
+            sendImageMessageAsync("src/main/resources/images/final_pic.jpg");
+
         }
     }
 
+    private void handleMessage(Update updateEvent) {
+        String messageText = updateEvent.getMessage().getText();
+        String firstName = updateEvent.getMessage().getFrom().getFirstName();
+
+        if (messageText.equals("/start")) {
+            handleStartCommand(firstName);
+        }
+    }
+
+    private void handleStartCommand(String firstName) {
+        sendImageMessageAsync("src/main/resources/images/hello.jpg");
+        String greetingMessage = "*Привет*, будущий программист " + firstName + "!";
+        String message = "Хочешь ли ты узнать по истене удивительную историю?";
+        sendTextMessageAsync(greetingMessage);
+        sendTextMessageAsync(message, Map.of("Да", "yes", "Нет", "no"));
+    }
 
 
     public static void main(String[] args) throws TelegramApiException {
